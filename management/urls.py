169b00231efe724bb32bb89388e539cd21c7fc57
Apollo Jain <apollojain@calvisitor-10-105-156-64.calvisitor.1918.berkeley.edu>
2017-04-24 20:47:04 -0700

    added about page

--- management/urls.py
+++ management/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 
 from . import views
 
@@ -10,6 +11,7 @@ urlpatterns = [
     url(r'^signup/$', views.create, name='signup'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^$', views.index, name='home'),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^(?P<event_id>[0-9]+)/$', views.detail, name='event_detail'),
     url(r'^toggle/(?P<event_id>[0-9]+)/$', views.toggle, name='toggle'),
     
